9b6a5a57c259f69e2741a6f18a6ed61c06eafd26
Ueliton Alves Dos Santos <ueliton@disroot.org>
2023-06-06 21:03:38 -0300

    Add files via upload

---
+++ videorecorder.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Live Yes
+#Manufacturer ANJIA
+#Model AJ0186
+#Firmware v180552.1110
+HOST='192.168.66.1'
+USER=admin
+PASS=123456
+SAVEDIR="$HOME/Onvier"
+
+#Save video from rtsp source in 5 minute chunks
+mkdir -p $SAVEDIR
+ffmpeg -i rtsp://${USER}:${PASS}@${HOST}:8554/profile000... -s 320x240 -b:v 256k -r 15 -f segment -segment_time 300 -reset_timestamps 1 -strftime 1 ${SAVEDIR}/Videos-%Y%m%d_%H%M%S.mkv
